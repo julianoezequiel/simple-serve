e49f785c7fb637e84bc1a0718a87b9cead5f4f6e
julianoezequiel <julianoezequiel@mail.com>
2017-07-30 14:17:55 -0300

    dash

--- src/main/java/com/api/filter/TokenFilter.java
+++ src/main/java/com/api/filter/TokenFilter.java
@@ -26,35 +26,38 @@ import io.jsonwebtoken.Jwts;
 @Component
 public class TokenFilter extends GenericFilterBean {
 
-//	private final static Logger LOGGER = LoggerFactory.getLogger(TokenFilter.class.getName());
+	// private final static Logger LOGGER =
+	// LoggerFactory.getLogger(TokenFilter.class.getName());
 
 	@Autowired
 	private UsuarioService usuarioService;
-	
+
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
 			throws IOException, ServletException {
 		HttpServletRequest req = (HttpServletRequest) request;
 		String token = req.getHeader("Authorization");
+		String method = req.getMethod();
+		if (method != null && !method.equalsIgnoreCase("OPTIONS")) {
+			if (token == null || token.equals("") || token.length() < 10) {
+				((HttpServletResponse) response).sendError(HttpServletResponse.SC_UNAUTHORIZED,
+						"Token inexistente ou inválido");
+			} else {
 
-		if (token == null || token.equals("") || token.length() < 10) {
-			((HttpServletResponse) response).sendError(HttpServletResponse.SC_UNAUTHORIZED,
-					"Token inexistente ou inválido");
-		} else {
+				// token valido
+				try {
+					Claims body = Jwts.parser().setSigningKey(CONSTANTES.AUTH_KEY).parseClaimsJws(token).getBody();
 
-			// token valido
-			try {
-				Claims body = Jwts.parser().setSigningKey(CONSTANTES.AUTH_KEY).parseClaimsJws(token).getBody();
+					Usuario usuario = this.usuarioService.buscarPorEmail(body.getSubject());
+					if (usuario == null) {
+						throw new ServiceException(HttpStatus.UNAUTHORIZED, "Usuário ou senha inválidos");
+					}
 
-				Usuario usuario = this.usuarioService.buscarPorEmail(body.getSubject());
-				if(usuario==null){
-					throw new ServiceException(HttpStatus.UNAUTHORIZED, "Usuário ou senha inválidos");
+				} catch (JwtException | ClassCastException | ServiceException e) {
+					((HttpServletResponse) response).sendError(HttpServletResponse.SC_UNAUTHORIZED, "não autorizado");
 				}
 
-			} catch (JwtException | ClassCastException | ServiceException e) {
-				((HttpServletResponse) response).sendError(HttpServletResponse.SC_UNAUTHORIZED, "não autorizado");
 			}
-
 		}
 		filterChain.doFilter(request, response);
 
